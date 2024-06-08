7ced6221c32e2d0590837aec09e193893f49049a
Krishna <krishnamahato84044@gmail.com>
2024-06-09 03:45:01 +0530

    final-commit

--- script.js
+++ script.js
@@ -19,14 +19,39 @@ var data;
     createEmploye.addEventListener("click",()=>{
         addEmployeeModal.style.display = "flex";
     })
+    addEmployeeModal.addEventListener("click",(e)=>{
+        if(e.target.className === "addEmployee"){
+            addEmployeeModal.style.display = "none";
+        }
+    })
+    addEmployeeForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const formData = new FormData(addEmployeeForm);
+        const values = [...formData]
+        console.log(values);
+        let employeData = {}
+
+        values.forEach((value)=>{
+            employeData[value[0]] = value[1];
+        })
+        employeData.id = Math.random(3)*10000 ||data[data.length-1].id+1 || 1000;
+        employeData.imageUrl = employeData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+        console.log(employeData)
+        data.push(employeData);
+        console.log(data[data.length-1]);
+        addEmployeeModal.style.display = "none";
+        renderList();
+        addEmployeeForm.reset();
+    })
 
 
 
 
 
 
+    let singleData;
     employeList.addEventListener('click',(e)=>{
-        let singleData;
+
         if(e.target.tagName === 'SPAN' && e.target.id != singleData?.id){            
             e.target.classList.add('selected');
             data.forEach((d)=>{
@@ -38,10 +63,35 @@ var data;
             renderEmployeInfo(singleData);
         }
 
+        if(e.target.tagName === 'I'){
+            let prevEmp = e.target.parentNode.id;
+            data = data.filter((emp) => String(emp.id) !== e.target.parentNode.id);
+            
+            // console.log(singleData)
+            if(singleData){
+                // console.log(prevEmp , " -- ",singleData.id)
+                if(prevEmp == singleData.id){
+                    renderEmployeInfo(data[0])
+                }
+                else {
+                    renderEmployeInfo(singleData)
+                }
+            }
+            else {
+                renderEmployeInfo(data[0])
+                singleData = data[0];
+
+            }
+            employeList.innerHTML = "";
+            renderList();
+        }
+
+        
+
     })
-    const renderEmployeInfo = (info) =>{
-        console.log("Calling rendoEmployeINfo")
-        employInfo.innerHTML = `
+    const renderEmployeInfo = (info  ) => {
+        // console.log("Calling rendoEmployeINfo")
+        if(info)employInfo.innerHTML = `
         <div class="emp--info__container">
             <img  src="${info.imageUrl}" class="employe-img" width="50px" />
             <p class="emp--name">
@@ -56,12 +106,12 @@ var data;
             <span>Age - ${info.age}</span>
         </div>
         `
+        else employInfo.innerHTML = `<h2>No Data Found</h1>`
     }
+    renderEmployeInfo(data[0] || [])
     
     const renderList = () =>{
-        
-
-        data.forEach((ele) => {
+        data?.forEach((ele) => {
             const employe = document.createElement('span');
             employe.classList.add('employe--name')
             employe.innerHTML = `${ele.firstName} ${ele.lastName} <i class="delete--btn">❌</i> `
@@ -71,10 +121,5 @@ var data;
         });
     }
     renderList();
-
-
-
-
-
 })()
 
